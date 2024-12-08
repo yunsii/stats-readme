4090f9ba4136a402344a0c67468ad0980d5b2e8f
Yuns <yuns.xie@qq.com>
2024-12-08 18:06:50 +0800

    test: calculateRank should work correctly

---
+++ __tests__/user-stats.test.ts
@@ -0,0 +1,29 @@
+import { it } from '@jest/globals'
+
+import { calculateRank } from '../src/utils/user-stats'
+
+it('should calculate rank correctly', () => {
+  const stats = {
+    name: 'Yuns',
+    totalRepos: 153,
+    totalReviews: 2,
+    totalPRs: 84,
+    totalCommits: 4191,
+    totalCommitsInLastYear: 683,
+    totalIssues: 201,
+    totalStars: 857,
+    followers: 144,
+    contributedToInLastYear: 26
+  }
+
+  calculateRank({
+    totalRepos: stats.totalRepos,
+    commits: stats.totalCommits,
+    isAllCommits: true,
+    followers: stats.followers,
+    prs: stats.totalPRs,
+    issues: stats.totalIssues,
+    reviews: stats.totalReviews,
+    stars: stats.totalStars
+  })
+})
